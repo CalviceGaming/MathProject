cf1071fc75729d4525bda1a2c9bb4bf9a3ea72ab
CalviceGaming <diogo.pereira.d24@gmail.com>
2025-04-04 23:41:59 +0100

    more cubes

--- Assets/Scripts/CubeMeshCreator.cs
+++ Assets/Scripts/CubeMeshCreator.cs
@@ -25,7 +25,7 @@ public class CubeMeshCreator : MonoBehaviour
     {
         for(int i = 0; i < vertices.Length; i++)
         {
-            vertices[i] = RotateX(vertices[i], 0.03f);
+            //vertices[i] = RotateX(vertices[i], 0.03f);
         }
         UpdateMesh();
     }
@@ -43,33 +43,55 @@ public class CubeMeshCreator : MonoBehaviour
 
     void CreateMesh()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3(-0.5f, 0.5f, -0.5f), //0
-            new Vector3(0.5f, 0.5f, -0.5f),//1
-            new Vector3(-0.5f, -0.5f, -0.5f),//2
-            new Vector3(0.5f, -0.5f, -0.5f),//3
-            new Vector3(0.5f, 0.5f, 0.5f),//4
-            new Vector3(0.5f, -0.5f, 0.5f),//5
-            new Vector3(-0.5f, 0.5f, 0.5f),//6
-            new Vector3(-0.5f, -0.5f, 0.5f),//7
-        };
+        List<Vector3> startingVertices = new List<Vector3>();
+        
+        startingVertices.Add(new Vector3(-0.5f, 0.5f, -0.5f)); //0
+        startingVertices.Add(new Vector3(0.5f, 0.5f, -0.5f));//1
+        startingVertices.Add(new Vector3(-0.5f, -0.5f, -0.5f));//2
+        startingVertices.Add(new Vector3(0.5f, -0.5f, -0.5f));//3
+        startingVertices.Add(new Vector3(0.5f, 0.5f, 0.5f));//4
+        startingVertices.Add(new Vector3(0.5f, -0.5f, 0.5f));//5
+        startingVertices.Add( new Vector3(-0.5f, 0.5f, 0.5f));//6
+        startingVertices.Add(new Vector3(-0.5f, -0.5f, 0.5f));//7
 
-        triangles = new int[]
+        List<int> startingTriangles = new List<int>
         {
             0, 1, 2,
-            1,3,2,
-            1,4,3,
-            4,5,3,
-            0,6,4,
-            0,4,1,
-            0,7,6,
-            0,2,7,
-            6,7,4,
-            7,5,4,
-            2,3,5,
-            2,5,7
+            1, 3, 2,
+            1, 4, 3,
+            4, 5, 3,
+            0, 6, 4,
+            0, 4, 1,
+            0, 7, 6,
+            0, 2, 7,
+            6, 7, 4,
+            7, 5, 4,
+            2, 3, 5,
+            2, 5, 7
         };
+
+        for (int y = 1; y <= 5; y++)
+        {
+            int amount = 8 * y;
+            for (int i = amount - 8; i < amount; i++)
+            {
+                startingVertices.Add(startingVertices[i] + new Vector3(0, 0, 1));
+            }
+        
+            int amount2 = 36 * y;
+            for (int i = amount2 - 36; i < amount2; i++)
+            {
+                startingTriangles.Add(startingTriangles[i] + 8);
+            }
+        }
+
+        
+        vertices = startingVertices.ToArray();
+        triangles = startingTriangles.ToArray();
+        for (int i = 0; i < startingVertices.Count; i++)
+        {
+            
+        }
     }
 
     void UpdateMesh()
